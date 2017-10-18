c50480279baabe5b3e10390f6b9c03bcaab1b08b
Parv Kumar <parvathikumar92@gmail.com>
2017-10-18 02:48:53 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -40,9 +40,5 @@ describe('Fix the Scope', function() {
     });
   });
 
-  xdescribe('attemptTwoFavoriteCustomers()', function() {
-    it('unsuccessfully tries to declare favoriteCustomer with let two times', function() {
-      expect(attemptTwoFavoriteCustomers).to.throw(SyntaxError);
-    });
-  });
+  
 });
